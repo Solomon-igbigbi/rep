b8441f8d69afabbcac8d7d348469c2eddfafb0fc
Israel David <77063876+israel1281@users.noreply.github.com>
2022-08-17 03:52:50 -0700

    auth pages

---
+++ pages/Register.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { Text } from "../atoms";
+
+const Register: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      
+      <main>
+        <h1>Register</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Register;
