35c48baadab1c75431ea7be88b30e4cfe4e15320
Israel David <77063876+israel1281@users.noreply.github.com>
2022-08-17 03:50:44 -0700

    auth pages

---
+++ pages/Login.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { Text } from "../atoms";
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+      </Head>
+      
+      <main>
+        <h1>Login</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Login;
