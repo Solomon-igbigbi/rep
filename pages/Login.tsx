4e43a83e833916dd33054e91c9ce224cfdef417e
israel1281 <kidoski1@gmail.com>
2022-08-18 04:28:37 -0700

    init commit

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,17 +1,80 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
+import styles from "../styles/Login.module.scss";
+import Group from "../assests/png/Group.png";
+import FacebookIcon from "../assests/png/facebookImg.png";
+import GoogleIcon from "../assests/png/googleImg.png";
+import LinkedinIcon from "../assests/png/linkedinImg.png";
+import { Button }  from "../atoms";
 import { Text } from "../atoms";
 
 const Login: NextPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Login</title>
       </Head>
       
-      <main>
-        <h1>Login</h1>
+      <main className={styles.main}>
+        <div className={styles.loginCard}>
+          <div className={styles.cardLeft}>
+            <div className={styles.topLogo}>
+              <Image
+                src={Group}
+                alt="reprezentme logo"
+                loading="lazy"
+              />
+            </div>
+
+            <div className={styles.form}>
+              <div className={styles.bemVindos}>
+                <span className={styles.signin}>Sign in to</span>
+                <span className={styles.text2}>Reprezent</span>
+                <span className={styles.text3}>Me</span>
+              </div>
+
+              <div className={styles.socials}>
+                <Image
+                  src={FacebookIcon}
+                  alt="facebook icon"
+                  loading="lazy"
+                />
+                <Image
+                  src={GoogleIcon}
+                  alt="google icon"
+                  loading="lazy"
+                />
+                <Image
+                  src={LinkedinIcon}
+                  alt="linkedin icon"
+                  loading="lazy"
+                />
+              </div>
+
+              <div className={styles.username}>
+                  <input type="text" placeholder="Username" />
+                </div>
+                <div className={styles.password}>
+                  <input type="password" placeholder="Password" />
+                </div>
+
+                <div className={styles.forgot}>
+                  <Link href="#">Forgot Password?</Link>
+                </div>
+
+                <button>Login now</button>
+            </div>
+          </div>
+          <div className={styles.cardRight}>
+            <span className={styles.text1}>Hello, Friends</span>
+            <span className={styles.text2}>Enter your personal details and strat journy with us</span>
+            <button>
+              SIGN UP
+            </button>
+          </div>
+        </div>
       </main>
     </div>
   )
