446b8593932d01b5f5dec322b42b5b50794805b8
israel1281 <kidoski1@gmail.com>
2022-09-01 02:52:05 -0700

    polls selection added

--- pages/polls/index.tsx
+++ pages/polls/index.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from "next";
 import Image from "next/image";
 import React, { ChangeEvent, useState } from "react";
-import { IconInput, Text } from "../../atoms";
+import { Progress, Rate } from 'antd'
+import { IconInput, Text, Card } from "../../atoms";
 import { ChatHead } from "../../molecules";
 import { ChatBar, Layout } from "../../template";
 import styles from "./polls.module.scss";
@@ -90,7 +91,219 @@ const Polls: NextPage = () => {
             />
             <div className={styles.polls_left_body}>
               {screen.toLocaleLowerCase() === "constituency polls" && (
-                <div>In Progress</div>
+                <div className={styles.constituency_wrapper}>
+                  <div className={styles.constituency_polls_left}>
+                    <Text variant="h5" bold>How would you rate performance of the Benue south Senator this month?</Text>
+                    <div className={styles.ratings_card}>
+                      <Card style={{
+                        width: "100%"
+                      }}>
+                        <div style={{
+                          width: "100%",
+                          height: "20px"
+                        }}>
+                          <div style={{
+                            width: "40%",
+                            height: "30px",
+                            backgroundColor: "#f2c94c",
+                            float: "right",
+                            borderBottomLeftRadius: "30px",
+                            borderTopRightRadius: "10px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                          }}>
+                            <Rate style={{ 
+                              fontSize: "10px", 
+                              color: "#000000"
+                               }} allowHalf defaultValue={2.5} />
+                          </div>
+                        </div>
+                        <div style={{
+                          padding: "0px 40px 5px 30px"
+                        }}>
+                          <Text style={{
+                            marginBottom: "-2px"
+                          }} variant="h5" bold>Excellent</Text>
+                          <Progress
+                            strokeColor={{
+                              '0%': '#108ee9',
+                              '100%': '#87d068',
+                            }}
+                            percent={30}
+                            showInfo={false}
+                          />
+                          <Text variant="p-sm">8 Votes</Text>     
+                        </div>
+                      </Card>
+                      <Card style={{
+                        width: "100%"
+                      }}>
+                        <div style={{
+                          width: "100%",
+                          height: "20px"
+                        }}>
+                          <div style={{
+                            width: "40%",
+                            height: "30px",
+                            backgroundColor: "#f2c94c",
+                            float: "right",
+                            borderBottomLeftRadius: "30px",
+                            borderTopRightRadius: "10px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                          }}>
+                            <Rate style={{ 
+                              fontSize: "10px", 
+                              color: "#000000"
+                               }} allowHalf defaultValue={4} />
+                          </div>
+                        </div>
+                        <div style={{
+                          padding: "0px 40px 5px 30px"
+                        }}>
+                          <Text style={{
+                            marginBottom: "-2px"
+                          }} variant="h5" bold>Good</Text>
+                          <Progress
+                            strokeColor={{
+                              '0%': '#9fcc2f',
+                              '100%': '#9fcc2f',
+                            }}
+                            percent={10}
+                            showInfo={false}
+                          />
+                          <Text variant="p-sm">4 Votes</Text>     
+                        </div>
+                      </Card>
+                      <Card style={{
+                        width: "100%"
+                      }}>
+                        <div style={{
+                          width: "100%",
+                          height: "20px"
+                        }}>
+                          <div style={{
+                            width: "40%",
+                            height: "30px",
+                            backgroundColor: "#000e80",
+                            float: "right",
+                            borderBottomLeftRadius: "30px",
+                            borderTopRightRadius: "10px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                          }}>
+                            <Rate style={{ 
+                              fontSize: "10px", 
+                              color: "#f2c94c"
+                               }} allowHalf defaultValue={3} />
+                          </div>
+                        </div>
+                        <div style={{
+                          padding: "0px 40px 5px 30px"
+                        }}>
+                          <Text style={{
+                            marginBottom: "-2px"
+                          }} variant="h5" bold>Average</Text>
+                          <Progress
+                            strokeColor={{
+                              '0%': '#edde40',
+                              '100%': '#edde40',
+                            }}
+                            percent={50}
+                            showInfo={false}
+                          />
+                          <Text variant="p-sm">58 Votes</Text>     
+                        </div>
+                      </Card>
+                      <Card style={{
+                        width: "100%"
+                      }}>
+                        <div style={{
+                          width: "100%",
+                          height: "20px"
+                        }}>
+                          <div style={{
+                            width: "40%",
+                            height: "30px",
+                            backgroundColor: "#f2c94c",
+                            float: "right",
+                            borderBottomLeftRadius: "30px",
+                            borderTopRightRadius: "10px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                          }}>
+                            <Rate style={{ 
+                              fontSize: "10px", 
+                              color: "#000000"
+                               }} allowHalf defaultValue={2} />
+                          </div>
+                        </div>
+                        <div style={{
+                          padding: "0px 40px 5px 30px"
+                        }}>
+                          <Text style={{
+                            marginBottom: "-2px"
+                          }} variant="h5" bold>Below Average</Text>
+                          <Progress
+                            strokeColor={{
+                              '0%': '#ee9f25',
+                              '100%': '#ee9f25',
+                            }}
+                            percent={20}
+                            showInfo={false}
+                          />
+                          <Text variant="p-sm">15 Votes</Text>     
+                        </div>
+                      </Card>
+                      <Card style={{
+                        width: "100%"
+                      }}>
+                        <div style={{
+                          width: "100%",
+                          height: "20px"
+                        }}>
+                          <div style={{
+                            width: "40%",
+                            height: "30px",
+                            backgroundColor: "#f2c94c",
+                            float: "right",
+                            borderBottomLeftRadius: "30px",
+                            borderTopRightRadius: "10px",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                          }}>
+                            <Rate style={{ 
+                              fontSize: "10px", 
+                              color: "#000000"
+                               }} allowHalf defaultValue={1} />
+                          </div>
+                        </div>
+                        <div style={{
+                          padding: "0px 40px 5px 30px"
+                        }}>
+                          <Text style={{
+                            marginBottom: "-2px"
+                          }} variant="h5" bold>Poor</Text>
+                          <Progress
+                            strokeColor={{
+                              '0%': '#e53a1b',
+                              '100%': '#e53a1b',
+                            }}
+                            percent={20}
+                            showInfo={false}
+                          />
+                          <Text variant="p-sm">21 Votes</Text>     
+                        </div>
+                      </Card>
+                    </div>
+                  </div>
+                  <div className={styles.constituency_polls_right}>hello</div>
+                </div>
               )}
               {screen.toLocaleLowerCase() === "other polls" && (
                 <div className={styles.vote}>
