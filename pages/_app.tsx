cda5706c7b53b1c1c1f92b1182dc9b076b87e6c5
solomon-igbigbi <solomonigbigbi07@gmail.com>
2022-09-06 11:04:47 +0100

    git commit -m added some changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,18 @@ import type { AppProps } from "next/app";
 import "antd/dist/antd.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  // const [state, setState] = useState<{ width: number; height: number }>({});
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     setState({ width: window.innerWidth, height: window.innerHeight });
+  //   }
+  // });
+  return (
+    <>
+      {/* <div>{state?.width}</div> */}
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
